759652143cef6c23da01160a8fb724effb92ce02
NinoBk09 <ninobabickovacic@gmail.com>
2026-05-07 13:30:55 +0200

    Stevec_Zmag
    Dodal sem Števec zmag, ki štejejo zmage vsakega igralca in izenačitve, te nato tudi shrani, tako da se ob naslednjem nalaganju štejejo dalje.

--- krizec_krozec_asksave.py
+++ krizec_krozec_asksave.py
@@ -5,7 +5,7 @@ from tkinter.filedialog import askopenfile
 from tkinter import messagebox
 
 def klik(i):    
-    global gumbi, igralec, seznam, d
+    global gumbi, igralec, seznam, d, player1, player2, izenaceno
     
     if gumbi[i]["text"] == "":
         seznam[i] = igralec
@@ -18,7 +18,10 @@ def klik(i):
             igralec = "X"
         d = {
                  'navrsti': igralec,
-                 'stanje': seznam
+                 'stanje': seznam,
+                 'zmaga_X': player1,
+                 'zmaga_y': player2,
+                 'izenaceno': dif
             }
     izenaceno()        
     zmagovalec=zmaga()
@@ -28,9 +31,18 @@ def klik(i):
             g.config(state="disable")
 
 def zmaga():
+    global player1, player2
     kombinacije = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     for k in kombinacije:
         if gumbi[k[0]]["text"] == gumbi[k[1]]["text"] == gumbi[k[2]]["text"] != "":
+            if igralec == "O":
+                player1 += 1
+                d['zmaga_x'] = player1
+                label_x.config(text=f"X zmage: {player1}")
+            elif igralec == "X":
+                player2 += 1
+                d['zmaga_y'] = player2
+                label_o.config(text=f"O zmage: {player2}")
             return gumbi[k[0]]["text"]
     return None
 
@@ -46,8 +58,11 @@ def reset():
     print(seznam)
         
 def izenaceno():
-    global seznam
+    global seznam, dif, d
     if seznam.count("") == 0:
+        dif += 1
+        d['izenaceno'] = dif
+        label_izenaceno.config(text=f"izenačeno: {dif}")
         label.config(text="Izenačeno", bg="yellow")
         for g in gumbi:
             g.config(state="disable")
@@ -61,14 +76,21 @@ def shrani():
     label.config(text="Shranjeno", bg="lightgreen")
 
 def nalozi():
-    global d, igralec, seznam
+    global d, igralec, seznam, dif, player2, player1
     try:
         file = askopenfile(filetypes=[('JSON dokument', '*.json'), ('Vse datoteke', '*.*')])
         d = json.load(file)
         igralec = d['navrsti']
-        seznam = d['stanje']   
-        for i in range(9):
-            gumbi[i].config(text=seznam[i],state="normal")
+        seznam = d['stanje']
+        if not zmaga:
+            for i in range(9):
+                gumbi[i].config(text=seznam[i],state="normal")
+        label_izenaceno.config(text=f"izenačeno: {d['izenaceno']}")
+        label_o.config(text=f"O zmage: {d['zmaga_y']}")
+        label_x.config(text=f"X zmage: {d['zmaga_x']}")
+        player1 = d['zmaga_x']
+        player2 = d['zmaga_y']
+        dif = d['izenaceno']
         
         label.config(text="datoteka uspešno naložena", bg="lightgreen")
     except FileNotFoundError:
@@ -85,6 +107,9 @@ def nalozi():
 okno = tk.Tk()
 okno.title("polje")
 
+player1 = 0
+player2 = 0
+dif = 0
 gumbi=[]
 igralec = "X"
 d=0
@@ -113,5 +138,18 @@ okvir_label.pack(pady=10)
 
 label = tk.Label(okvir_label, text="", pady="10")
 label.grid(column=2, row=1)
+
+okvir_rezultat = tk.Frame(okno)
+okvir_rezultat.pack()
+
+label_x = tk.Label(okvir_rezultat, text=f"X zmage: {player1}", height=3, width=9,)
+label_x.grid(row=0, column=0)
+
+label_o = tk.Label(okvir_rezultat, text=f"O zmage: {player2}", height=3, width=9,)
+label_o.grid(row=1, column=0)
+
+label_izenaceno = tk.Label(okvir_rezultat, text=f"izenačeno: {dif}", height=3, width=9,)
+label_izenaceno.grid(row=2, column=0)
+
 seznam = ["","","","","","","","",""]
 okno.mainloop()
